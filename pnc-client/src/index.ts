3044eb5bbfe110312260f966a7b86f09f3d07cd7
Alex Creasy <acreasy@redhat.com>
2018-08-23 11:53:02 +0100

    bootstrap.2

--- pnc-client/src/index.ts
+++ pnc-client/src/index.ts
@@ -1,5 +1,11 @@
-const world = 'world';
+import fetch from "isomorphic-fetch";
+import { Person } from "./Person";
 
-export function hello(entity: string = world): string {
-    return `Hello ${entity}! `;
-  }
+
+const URL_TO_FETCH = "https://raw.githubusercontent.com/project-ncl/pnc/master/ui/package.json";
+
+export function fetchPerson(): Promise<Person> {
+  return fetch(URL_TO_FETCH)
+      .then(resp => resp.json())
+      .then(person => person as Person);
+}
